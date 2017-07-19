9f5c4b23f0643a0937db42f6b5d023a6a05f4c8e
Leonardo <leomanancial@hotmail.com>
2017-07-18 22:44:37 -0300

    Concluida a verificação de horarios das tabelas

--- NextBus/engine/engine.js
+++ NextBus/engine/engine.js
@@ -11,18 +11,24 @@
           var min = myClock.getMinutes();
         }
         var now = hour+''+min;
-        //console.log(tabela);
-        //Tabela tem que ir para o Json separada
-        if(tabela == 'Cacula'){
-        var horaCacula = [425,445,505,525,545,605,625,640,705,725,745,805,845,910,925,1000,1025,1100,1120,1140,1200,1220,1240,1300,1320,1340,1400,1420,1440,1500,1520,1540,1600,1620,1645,1705,1725, 1745,1810,1830,1850,1915,1940,2020,2100,2140,2210,2245,2320,2355,0010];
-        }else{
-          $('#nb').text("Calma ta chegando");
-        }
         var mostrahora = []; //Segunda lista que é adicionada os horários maiores que a hora local
+        var tabelaR = new Array()    
         
-        for (var i =0; i <horaCacula.length;i++ ){ 
-          if( now <= horaCacula[i] ){ // Verifica todos os horários da Tabela igual ou maior a hora local
-            mostrahora.push(horaCacula[i]) // Adiciona a Segunda lista os horários acima da hora local
+        tabelaR['Cacula'] = [425,445,505,525,545,605,625,640,705,725,745,805,845,910,925,1000,1025,1100,1120,1140,1200,1220,1240,1300,1320,1340,1400,1420,1440,1500,1520,1540,1600,1620,1645,1705,1725, 1745,1810,1830,1850,1915,1940,2020,2100,2140,2210,2245,2320,2355,0010]
+        tabelaR['SBC'] = [425,445,505,525,545,605,625,640,705,725,745,805,845,910,925,1000,1025,1100,1120,1140,1200,1220,1240,1300,1320,1340,1400,1420,1440,1500,1520,1540,1600,1620,1645,1705,1725, 1745,1810,1830,1850,1915,1940,2020,2100,2140,2210,2245,2320,2355,0010]
+        var tabelaC = []
+        //Localiza a tabela clicada e manda para comparar horario
+        for( x in tabelaR){
+          console.log(x)
+          console.log(tabela)
+          if(x == tabela ){
+            tabelaC = tabelaR[x]
+          }
+        }
+       //Compara a tabela com horario do equipamento
+        for (var i =0; i <tabelaC.length;i++ ){ 
+          if( now <= tabelaC[i] ){ // Verifica todos os horários da Tabela igual ou maior a hora local
+            mostrahora.push(tabelaC[i]) // Adiciona a Segunda lista os horários acima da hora local
           }
         }
         if( mostrahora[0] < 999){//verifica se a hora tem menos que tres digitos pegar no substring
